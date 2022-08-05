c1fc2f2362b6f1accc367be41b13011734c93fd6
Matheus <matheus.patricio@sptech.school>
2022-08-05 11:21:49 -0300

    Adição de novos atributos

---
+++ projetos/src/entidades/Endereco.java
@@ -0,0 +1,10 @@
+package entidades;
+
+public class Endereco {
+    private String estado;
+    private String Cidade;
+    private String nomeRua;
+    private String bairro;
+    private int cep;
+
+}
--- projetos/src/entidades/Pessoa.java
+++ projetos/src/entidades/Pessoa.java
@@ -1,4 +1,64 @@
 package entidades;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Pessoa {
+    private String nome;
+    private Date dtNascimento;
+    private Endereco endereco;
+    private String CPF;
+
+    public Pessoa(){};
+    public Pessoa(String nome, Date dtNascimento, Endereco endereco, String CPF) {
+        this.nome = nome;
+        this.dtNascimento = dtNascimento;
+        this.endereco = endereco;
+        this.CPF = CPF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDtNascimento() {
+        return dtNascimento;
+    }
+
+    public void setDtNascimento(Date dtNascimento) {
+        this.dtNascimento = dtNascimento;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return nome.equals(pessoa.nome) && CPF.equals(pessoa.CPF);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, CPF);
+    }
 }
